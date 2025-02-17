945fc81b2e389ccca867836b509c8cbe36eea4ae
deco-jpg <srdaniloh@gmail.com>
2025-02-17 18:42:06 +0000

    Salvando

---
+++ 2.tipagem estatica.py
@@ -0,0 +1,16 @@
+import os
+os.system("clear") 
+
+#Definição de tipos obrigatoria.
+
+nome: str #Equivalente a caracter
+idade: int #Equivalente a inteiro
+altura: float #Equivalente a real
+
+nome = "Danilo"
+idade = 22
+altura = 1.80
+
+print (nome)
+print (idade)
+print (altura)
---
+++ 3.tipagem_dinamica.py
@@ -0,0 +1,13 @@
+import os
+os.system("clear")
+
+# Definição de tipos automatica.
+
+
+nome = "Danilo"
+idade = 22
+altura = 1.80
+
+print (nome)
+print (idade)
+print (altura)
