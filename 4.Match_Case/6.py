d46c2c453eb1be3b02a9ed9d0d1478c146bd0cf8
deco-jpg <srdaniloh@gmail.com>
2025-02-26 20:26:31 -0300

    Atividade_compras.py

--- 4.Match_Case/6.py
+++ 4.Match_Case/6.py
@@ -1,5 +1,5 @@
 import os
-os.system ("clear")
+os.system ("cls")
 
 #Entrada
 valor = float (input("Digite o valor do produto: "))
@@ -7,25 +7,75 @@ forma_pagar = int (input("""
 1- A vista
 2- A prazo
 Digite a opção do pagamento Pagamento: """))
-#Processamento
+
 match forma_pagar:
     case 1:
         desconto = valor * 0.10
+        valor_total = valor - desconto  
         print ("Pagamento à vista")
-        
+        print (f"Valor do produto: {valor}")
+        print (f"Forma de pagamento: A vista")
+        print (f"Valor do desconto: {desconto}")
+        print (f"Total a pagar: {valor_total}")
     case 2:
-        parcela = int (input("Digite a quantidade de parcelas: "))
-        match parcela:
-            case 1:
-                print ("1 parcela")
-            case 2:
-                print ("2 parcelas")
-            case 3:
-                print ("3 parcelas")
-            case 4:
-                print ("4 parcelas")
-            case 5:
-                print ("6 parcelas")
-            case _:
-                print ("Opção Inválida")
+        desconto = 0
+        parcelas = int (input("Escolha a quantidade de parcelas: "))
+    case _:
+        print ("Opção inválida!")
 
+
+if 'parcelas' in locals() or 'parcelas' in globals():
+    div_parcela = valor/parcelas
+
+
+    match parcelas:    
+        case 1:
+            print ("Pagamento de uma parcela")
+            print (f"Valor do produto: {valor}")
+            print (f"Forma de pagamento: A prazo")
+            print (f"Quantidade de parcelas: {parcelas}")
+            print (f"Valor por parcela: {div_parcela}")
+            print (f"Total a pagar: {valor}")
+        case 2:
+            print ("Pagamento de duas parcelas")
+            print (f"Valor do produto: {valor}")
+            print (f"Forma de pagamento: A prazo")
+            print (f"Quantidade de parcelas: {parcelas}")
+            print (f"Valor por parcela: {div_parcela}")
+            print (f"Total a pagar: {valor}")
+        case 3:
+            print ("Pagamento de três parcelas")
+            print (f"Valor das parcelas: {div_parcela}")
+            print (f"Valor do produto: {valor}")
+            print (f"Forma de pagamento: A prazo")
+            print (f"Quantidade de parcelas: {parcelas}")
+            print (f"Valor por parcela: {div_parcela}")
+            print (f"Total a pagar: {valor}")
+        case 4:
+            print ("Pagamento de quatro parcelas")
+            print (f"Valor das parcelas: {div_parcela}")
+            print (f"Valor do produto: {valor}")
+            print (f"Forma de pagamento: A prazo")
+            print (f"Quantidade de parcelas: {parcelas}")
+            print (f"Valor por parcela: {div_parcela}")
+            print (f"Total a pagar: {valor}")
+        case 5:
+            print ("Pagamento de cinco parcelas")
+            print (f"Valor das parcelas: {div_parcela}")
+            print (f"Valor do produto: {valor}")
+            print (f"Forma de pagamento: A prazo")
+            print (f"Quantidade de parcelas: {parcelas}")
+            print (f"Valor por parcela: {div_parcela}")
+            print (f"Total a pagar: {valor}")
+        case 6:
+            print ("Pagamento de seis parcelas")
+            print (f"Valor das parcelas: {div_parcela}")
+            print (f"Valor do produto: {valor}")
+            print (f"Forma de pagamento: A prazo")
+            print (f"Quantidade de parcelas: {parcelas}")
+            print (f"Valor por parcela: {div_parcela}")
+            print (f"Total a pagar: {valor}")
+        case _:
+            print ("Opção inválida!")
+else:
+     print("Número de parcelas não definido.")
