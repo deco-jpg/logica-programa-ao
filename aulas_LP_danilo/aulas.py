1226e6b030038184d66b707d291dda3f81fcb869
deco-jpg <srdaniloh@gmail.com>
2025-03-17 15:41:12 -0300

    Add files via upload

---
+++ aulas_LP_danilo/aulas.py
@@ -0,0 +1,16 @@
+import os
+os.system ("cls")
+
+#for o in range(1, 11):
+    #tabuada = 5 * o
+    #print (f"05 X {o}: {tabuada} ")
+
+
+numero = int (input("Digite um número: "))
+for i in range (1, 11):
+    print (f"{numero} X {i} = {numero * i} ")
+
+
+
+
+
---
+++ aulas_LP_danilo/gerar_numeros_pares.py
@@ -0,0 +1,10 @@
+import os
+os.system ("cls")
+
+#print ("So numeros pares:")
+#for i in range (100, 121, 2):
+    #print (i)
+
+for i in range (100, 121):
+    if i % 2 == 0:
+        print (i)
