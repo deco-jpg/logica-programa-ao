f4fb8e51f2743a3ee2ecdb85556cd22176fe3bec
deco-jpg <srdaniloh@gmail.com>
2025-04-02 16:55:14 -0300

    Add files via upload

---
+++ FUNÇÕES/1.sem_funcao.py
@@ -0,0 +1,17 @@
+import os
+os.system ("cls")
+
+print ("==SENAI==")
+os.system ("cls")
+
+print ("==SENAI==")
+nome = input("Digite seu nome: ")
+
+os.system ("cls")
+print ("==SENAI==")
+idade = int (input("DIgite sua idade: "))
+
+os.system ("cls")
+print ("==SENAI==")
+print (f"Nome: {nome}")
+print (f"Idade: {idade}")
