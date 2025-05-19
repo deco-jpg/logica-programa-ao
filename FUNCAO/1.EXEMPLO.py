cc7327dbc70e0d0a9024e910ef66731a537e2147
deco-jpg <srdaniloh@gmail.com>
2025-05-19 14:31:55 -0300

    save

--- FUNCAO/1.EXEMPLO.py
+++
@@ -1,9 +0,0 @@
-import os
-os.system ("cls")
-
-def saudacao(parametro):
-    print(f"Olá, {parametro}. Bem vinda ao nosso site.")
-
-nome_visitante = input ("Digite seu nome: ")
-
-saudacao(nome_visitante)
--- FUNCAO/exercicio7.py
+++
@@ -1,8 +0,0 @@
-import os
-os.system("cls")
-
-def metros_para_cm (metros):
-    return metros * 100
-metros = float (input("Digite o valor em metros: "))
-valor_cm = metros_para_cm (metros)
-print (f"{metros} metros é equivalente a: {valor_cm} centimetros")
